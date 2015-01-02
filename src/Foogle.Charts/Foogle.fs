56f455e9be6bd3c179409742153d0a8b29851088
Dominique Plante <dominique.plante@relayhealth.com>
2015-01-01 23:30:07 -0800

    fixed typo in documentation

--- src/Foogle.Charts/Foogle.fs
+++ src/Foogle.Charts/Foogle.fs
@@ -5,7 +5,7 @@
 // ------------------------------------------------------------------------------------------------
 
 /// Represents a value that can be passed as data point
-/// This can be any numeric type and vairous other .NET types
+/// This can be any numeric type and various other .NET types
 type value = System.IConvertible
 
 /// Represents a data source for the Foogle chart
